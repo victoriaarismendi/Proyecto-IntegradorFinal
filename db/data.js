71c431d7f46f754cb7d96bbf13d5c4a7f5f8a8a8
antoniareynal <reynala@udesa.edu.ar>
2022-04-05 12:04:51 -0300

    agregue imagenes a nuestra base de datos

--- db/data.js
+++ db/data.js
@@ -1,18 +1,18 @@
 let data = {
     user: "", 
     productos: [
-        {"id": 0, "producto": "collar Sofia", "precio": 1500, "material": "oro" },
-        {"id": 1, "producto": "pulsera Clara", "precio": 790, "material": "acero" },
-        {"id": 2, "producto": "anillo Victoria", "precio": 3450, "material": "diamante" },
-        {"id": 3, "producto": "llavero Antonia", "precio": 660, "material": "oro" },
-        {"id": 4, "producto": "hebilla Ignacio", "precio": 300, "material": "oro" },
-        {"id": 5, "producto": "tobillera Delfina", "precio": 850, "material": "plata" },
-        {"id": 6, "producto": "collar Lucia", "precio": 1300, "material": "acero" },
-        {"id": 7, "producto": "aros Lola", "precio": 790, "material": "plata" },
-        {"id": 8, "producto": "anillo Martina", "precio": 570, "material": "oro" },
-        {"id": 9, "producto": "cinturon Luis", "precio": 650, "material": "cuero" },
-        {"id": 10, "producto": "reloj Tomas", "precio": 1700, "material": "cuero" },
-        {"id": 11, "producto": "cadena Kiara", "precio": 1400, "material": "plata" }
+        {"id": 0, "producto": "collar Sofia", "precio": 1500, "material": "oro", "cover":"/images/products/collarprimero.jpg" },
+        {"id": 1, "producto": "pulsera Clara", "precio": 790, "material": "acero", "cover":"/images/products/pulserados.jpg" },
+        {"id": 2, "producto": "anillo Victoria", "precio": 3450, "material": "diamante", "cover": "/images/products/anillotres.jpg" },
+        {"id": 3, "producto": "llavero Antonia", "precio": 660, "material": "oro", "cover": "/images/products/llaverocuatro.jpg"},
+        {"id": 4, "producto": "Aros Ignacia", "precio": 300, "material": "oro", "cover": "/images/products/aritos5.jpg" },
+        {"id": 5, "producto": "tobillera Delfina", "precio": 850, "material": "plata", "cover": "/images/products/collar6.jpg"},
+        {"id": 6, "producto": "collar Lucia", "precio": 1300, "material": "acero", "cover": "/images/products/tobillera7.jpg"},
+        {"id": 7, "producto": "aros Lola", "precio": 790, "material": "plata", "cover": "/images/products/aros8.jpg" },
+        {"id": 8, "producto": "Set anillos Martina", "precio": 570, "material": "oro", "cover": "/images/products/anillos9.jpg"},
+        {"id": 9, "producto": "cinturon Ina", "precio": 650, "material": "oro", "cover": "/images/products/cinturon10.jpg"},
+        {"id": 10, "producto": "reloj Tomas", "precio": 1700, "material": "bronce", "cover": "/images/products/rejoj11.jpg"},
+        {"id": 11, "producto": "cadenas Kiara", "precio": 1400, "material": "plata", "cover": "/images/products/cadenas12.jpg" }
     ],
     comments: []
 }
