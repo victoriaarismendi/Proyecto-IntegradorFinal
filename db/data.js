e50f76c2ec73678695e7940fb1836027c3b83091
antoniareynal <reynala@udesa.edu.ar>
2022-04-06 19:02:14 -0300

    organizacion de la base de datos y pequenios cambios en las vistas del detalle de cada producto

--- db/data.js
+++ db/data.js
@@ -1,20 +1,102 @@
 let data = {
-    user: "", 
+    usarios: {}, 
     productos: [
-        {"id": 0, "producto": "collar Sofia", "precio": 1500, "material": "oro", "cover":"/images/products/collarprimero.jpg" },
-        {"id": 1, "producto": "pulsera Clara", "precio": 790, "material": "acero", "cover":"/images/products/pulserados.jpg" },
-        {"id": 2, "producto": "anillo Victoria", "precio": 3450, "material": "diamante", "cover": "/images/products/anillotres.jpg" },
-        {"id": 3, "producto": "llavero Antonia", "precio": 660, "material": "oro", "cover": "/images/products/llaverocuatro.jpg"},
-        {"id": 4, "producto": "Aros Ignacia", "precio": 300, "material": "oro", "cover": "/images/products/aritos5.jpg" },
-        {"id": 5, "producto": "tobillera Delfina", "precio": 850, "material": "plata", "cover": "/images/products/collar6.jpg"},
-        {"id": 6, "producto": "collar Lucia", "precio": 1300, "material": "acero", "cover": "/images/products/tobillera7.jpg"},
-        {"id": 7, "producto": "aros Lola", "precio": 790, "material": "plata", "cover": "/images/products/aros8.jpg" },
-        {"id": 8, "producto": "Set anillos Martina", "precio": 570, "material": "oro", "cover": "/images/products/anillos9.jpg"},
-        {"id": 9, "producto": "cinturon Ina", "precio": 650, "material": "oro", "cover": "/images/products/cinturon10.jpg"},
-        {"id": 10, "producto": "reloj Tomas", "precio": 1700, "material": "bronce", "cover": "/images/products/rejoj11.jpg"},
-        {"id": 11, "producto": "cadenas Kiara", "precio": 1400, "material": "plata", "cover": "/images/products/cadenas12.jpg" }
+        {
+            id: 0,
+            producto: "collar Sofia", 
+            precio: 1500, 
+            material: "oro",
+            imagen:"/images/products/collarprimero.jpg" 
+        },
+
+        {
+            id: 1, 
+            producto: "pulsera Clara",
+            precio: 790,
+            material: "acero",
+            imagen:"/images/products/pulserados.jpg" 
+        },
+
+        {
+            id: 2,
+            producto: "anillo Victoria", 
+            precio: 3450, 
+            material: "diamante", 
+            imagen: "/images/products/anillotres.jpg" 
+        },
+
+        {
+            id: 3, 
+            producto: "llavero Antonia", 
+            precio: 660,
+            material: "oro",
+            imagen: "/images/products/llaverocuatro.jpg"
+        },
+
+        {
+            id: 4, 
+            producto: "Aros Ignacia",
+            precio: 300,
+            material: "oro",
+            imagen: "/images/products/aritos5.jpg" 
+        },
+
+        {
+            id: 5, 
+            producto: "tobillera Delfina", 
+            precio: 850,
+            material: "plata",
+            imagen: "/images/products/collar6.jpg"
+        },
+
+        {
+            id: 6, 
+            producto: "collar Lucia",
+            precio: 1300, 
+            material: "acero",
+            imagen: "/images/products/tobillera7.jpg"
+        },
+
+        {
+            id: 7,
+            producto: "aros Lola", 
+            precio: 790, 
+            material: "plata",
+            imagen: "/images/products/aros8.jpg" 
+        },
+
+        {
+            id: 8, 
+            producto: "Set anillos Martina",
+            precio: 570,
+            material: "oro",
+            imagen: "/images/products/anillos9.jpg"
+        },
+
+        {
+            id: 9, 
+            producto: "cinturon Ina", 
+            precio: 650, 
+            material: "oro",
+            imagen: "/images/products/cinturon10.jpg"
+        },
+
+        {
+            id: 10, 
+            producto: "reloj Tomas",
+            precio: 1700, 
+            material: "bronce",
+            imagen: "/images/products/rejoj11.jpg"
+        },
+
+        {
+            id: 11, 
+            producto: "cadenas Kiara",
+            precio: 1400,
+            material: "plata",
+            imagen: "/images/products/cadenas12.jpg" }
     ],
-    comments: []
+    comentarios: [{}]
 }
 
 module.exports = data;
