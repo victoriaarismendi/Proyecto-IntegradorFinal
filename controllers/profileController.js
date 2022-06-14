0558dda04f8f43cb2d5e480f8f14fb87681925bb
delfinagalarza <dgalarzagraf@udesa.edu.ar>
2022-06-14 15:03:23 -0300

    no pudimos solucionar

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,16 +1,29 @@
 const db = require('../database/models');
 var joyas = require ('../db/data');
 
+
 const profileController = {
+
+myProfile: function(req, res) {
+        db.User.findByPk(req.session.user.id, { include: [ { association: 'joya' } ] })
+            .then(function (user) {
+                res.render('profile', { user });
+            })
+            .catch(function (error) {
+                res.send(error)
+            });
+    },
+
     index: function(req, res){
         db.User.findByPk(req.session.user.id, {includes: [{association: 'joya'}]})
         .then(usuario=>{
             return res.render('profile',{
                 usuario: usuario
-                
             })
         })
-        
+       
+
+
     },
     edit: function(req,res){
         if(req.session.user && req.session.user.id == req.params.id){
